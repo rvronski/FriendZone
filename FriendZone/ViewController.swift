78c663ba18b38cf558f470930db8848323968f56
rvronski <rvronski@gmail.com>
2023-05-13 20:45:29 +0500

    initial

---
+++ FriendZone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendZone
+//
+//  Created by ROMAN VRONSKY on 13.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
